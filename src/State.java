02acf4d95b390b0985f44b7b4bca843955487f31
Naktrem <atknatmrt@gmail.com>
2019-12-11 13:34:03 +0300

    Initial commit.

--- src/State.java
+++
@@ -1,9 +0,0 @@
-public interface State {
-    boolean canHeal();
-    boolean canKill();
-    boolean canMurder();
-    boolean canInvestigate();
-    boolean canProtect();
-
-    boolean canExecute();
-}
